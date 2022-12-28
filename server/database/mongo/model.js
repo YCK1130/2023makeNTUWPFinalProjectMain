33623076984e2858b23fa3ca66892573f90a377c
AustinC0831 <anonytp92@gmail.com>
2022-12-28 17:01:12 +0800

    我猜...model就這樣了?

--- server/database/mongo/model.js
+++ server/database/mongo/model.js
@@ -72,10 +72,43 @@ const requestSchema = new mongoose.Schema({
 
 const Request = conn.model("Request", requestSchema);
 
+const boardSchema = new mongoose.Schema({
+  boardID:{ 
+    type: Number,
+    required: true,
+  },
+  boardName:{
+    type: String,
+    required: true,
+  },
+  category:[{type: String}],
+  image:{ //link
+    type: String,
+  },
+  limit:{
+    type: Number,
+    required: true,
+  },
+  totalNum:{ //一開始的所有量
+    type: Number,
+    required: true,
+  },
+  remain:{ //剩下多少
+    type: Number,
+    required: true,
+  }, 
+  invoice:[{ //收據!被誰借走多少
+    group:{type: mongoose.Types.ObjectId, ref: "Team"},
+    number:{type: Number}
+  }],
+})
+
+const Board = conn.model("Board", boardSchema);
 // ========================================
 
 module.exports = {
   Team,
   Request,
+  Board,
   conn,
 };
